2e76bad6478d150c6ba92c4c78c5753e2b3f1752
Hongjun Chen <nanjingchj@gmail.com>
2021-01-04 11:00:22 -0500

    Initial Commit

---
+++ src/main/java/me/nanjingchj/imgrepo/ImageRepositoryApplication.java
@@ -0,0 +1,13 @@
+package me.nanjingchj.imgrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageRepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/nanjingchj/imgrepo/ImageRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nanjingchj.imgrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
